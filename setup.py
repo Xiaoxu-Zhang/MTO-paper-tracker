96782b196085d65f40ff1e7f52d29411bb992e2e
Xiaoxu Zhang <xxzhang_official@163.com>
2024-12-22 19:41:23 +0800

    feat: add entry script for Paper Watcher
    - Create setup.py file to define the entry point for Paper Watcher
    - Use Fire library to create a command-line interface
    - Initialize PaperWatcher class from src/main.py as the main entry

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!usr/bin/env/ python
+# -*- coding:utf-8 -*-
+"""
+@Author :Xiaoxu Zhang
+@Date   :2024/12/20
+"""
+from fire import Fire
+from src.main import PaperWatcher
+
+if __name__ == '__main__':
+    Fire(PaperWatcher)
